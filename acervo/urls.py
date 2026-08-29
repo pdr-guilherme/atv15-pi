70b61b8d0458643276fe149e48d90a3f9a7e5b37
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-08-29 12:47:09 -0300

    feat: adicionar listagem e detalhe de livros

--- acervo/urls.py
+++ acervo/urls.py
@@ -5,4 +5,6 @@ from acervo import views
 urlpatterns = [
     path("autores/", views.autor_list, name="autor_list"),
     path("autores/<int:pk>/", views.autor_detail, name="autor_detail"),
+    path("livros/", views.livro_list, name="livro_list"),
+    path("livros/<int:pk>/", views.livro_detail, name="livro_detail"),
 ]
--- acervo/views.py
+++ acervo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from acervo.models import Autor
+from acervo.models import Autor, Livro
 
 
 def autor_list(request):
@@ -13,3 +13,15 @@ def autor_detail(request, pk):
     autor = get_object_or_404(Autor, id=pk)
     context = {"autor": autor}
     return render(request, "acervo/autor_detail.html", context)
+
+
+def livro_list(request):
+    livros = Livro.objects.all()
+    context = {"livros": livros}
+    return render(request, "acervo/livro_list.html", context)
+
+
+def livro_detail(request, pk):
+    livro = get_object_or_404(Livro, id=pk)
+    context = {"livro": livro}
+    return render(request, "acervo/livro_detail.html", context)
