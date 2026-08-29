e80863733c55160d6e1db7b80c8808214d2ce48e
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-08-29 12:15:07 -0300

    feat: adicionar modelos ao site de administração

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from acervo.models import Autor, Livro
+
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "email")
+    search_fields = ("nome", "email")
+
+
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "autor", "editora")
+    search_fields = ("titulo", "autor", "resumo")
+    list_filter = ("editora",)
+
+
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Livro, LivroAdmin)
