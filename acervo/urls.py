6c4039a0584e67d1d8556f0d3a597c00132040ff
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-08-29 12:41:05 -0300

    feat: adicionar listagem e detalhe de autores

--- acervo/urls.py
+++ acervo/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from acervo import views
 
-urlpatterns = []
+urlpatterns = [
+    path("autores/", views.autor_list, name="autor_list"),
+    path("autores/<int:pk>/", views.autor_detail, name="autor_detail"),
+]
--- acervo/views.py
+++ acervo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from acervo.models import Autor
+
+
+def autor_list(request):
+    autores = Autor.objects.all()
+    context = {"autores": autores}
+    return render(request, "acervo/autor_list.html", context)
+
+
+def autor_detail(request, pk):
+    autor = get_object_or_404(Autor, id=pk)
+    context = {"autor": autor}
+    return render(request, "acervo/autor_detail.html", context)
